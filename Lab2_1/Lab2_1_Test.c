50d026d0523d09ce5cd1b350258002f82bc051a4
ClGelver <75281453+MaximSelhanovich@users.noreply.github.com>
2021-03-09 15:27:07 +0300

    Add files via upload

---
+++ Lab2_1/Lab2_1_Test.c
@@ -0,0 +1,66 @@
+#include "Lab2_1.h"
+
+#include <stdio.h>
+#include <assert.h>
+#include <float.h>
+
+void testFruitCosts() {
+
+    assert(tangerianCost(5) - TANGERIAN_PRICE_USD * 5  < DBL_EPSILON);
+    
+    assert(peachesCost(5) - PEACHES_PRICE_USD * 5  < DBL_EPSILON);
+    assert(grapeCost(5) - GRAPE_PRICE_USD * 5  < DBL_EPSILON);
+    printf("Costs test are succsesful.");
+}
+
+void testcalculateFruitCost() {
+
+    double testFruitCost[] = {5, 5, 5};
+    assert(calculateFruitCost(testFruitCost) - 17,1 < DBL_EPSILON);
+    printf("Fruit cost test comlete.");
+}
+
+void testCalculateDiscount() {
+
+    double testDiscount1[] = {5, 5, 5};
+    double testDiscount2[] = {100, 100, 100};
+    assert(calculateDiscount(testDiscount1) - 1.0 < DBL_EPSILON);
+    assert(calculateDiscount(testDiscount2) - 0.9 < DBL_EPSILON);
+    printf("Discaunt test complete.");    
+}
+
+void testCalculateDeliveryPrice() {
+
+    double testDelivery1 [] = {1, 1, 1};
+    double testDelivery2 [] = {5, 5, 5};
+    double testDelivery3 [] = {10, 10, 10};
+    assert(calculateDeliveryPrice (testDelivery1) - 1 < DBL_EPSILON);
+    assert(calculateDeliveryPrice (testDelivery2) - 3 < DBL_EPSILON);
+    assert(calculateDeliveryPrice (testDelivery3) - 30 < DBL_EPSILON);
+    printf("Delivery test complete.");
+}
+
+void testcalculateTotalOrderPrice() {
+
+    double testOrderPrice1[] = {1, 1, 1};
+    double testOrderPrice2[] = {5, 5, 5};
+    double testOrderPrice3[] = {10, 10, 10};
+    double testOrderPrice4[] = {100, 100, 100};
+    assert(calculateTotalOrderPrice(testOrderPrice1) - 4,42 < DBL_EPSILON);
+    assert(calculateTotalOrderPrice(testOrderPrice2) - 20,1 < DBL_EPSILON);
+    assert(calculateTotalOrderPrice(testOrderPrice3) - 64,2 < DBL_EPSILON);
+    assert(calculateTotalOrderPrice(testOrderPrice4) - 912 < DBL_EPSILON);
+    printf("Test order price complete.");
+}
+
+#undef main
+
+int main() {
+
+    testFruitCost();
+    testcalculateFruitCost();
+    testCalculateDiscount();
+    testCalculateDeliveryPrice();
+    testcalculateTotalOrderPrice();
+    printf("All tests are complited succesfuly.");
+}
---
+++ Lab2_2/Lab2_2_Test.c
@@ -0,0 +1,44 @@
+#include "Lab2_2.h"
+
+#include <stdio.h>
+#include <assert.h>
+#include <float.h>
+
+void testDegreesToRadians() {
+
+    assert(degreesToRadians(20) - 0.34906585 < DBL_EPSILON);
+    printf("Degrees to radians test complet");
+}
+
+void testSimplifyAngleDegrees() {
+
+    assert(simplifyAngleDegrees(390) - 30 < DBL_EPSILON);
+    assert(simplifyAngleDegrees(-390) + 30 < DBL_EPSILON);
+    assert(simplifyAngleDegrees(30) - 30 < DBL_EPSILON);
+    printf("Simplify angle in degrees test complet");
+}
+
+void testCalcSineDecompositionIterative() {
+
+    assert(calcSineDecompositionIterative(0.349065, 0.01) == 1);
+    printf("Sine decomposition iterative test complet");
+}
+
+void testCalcSineDecompositionRecursion() {
+
+    assert(calcSineDecompositionRecursion(0.349065, 0.0, 0.01) == 1);
+    printf("Sine decomposition iterative test complet");
+}
+
+#undef main
+
+int main() {
+
+    testDegreesToRadians();
+    testSimplifyAngleDegrees();
+    testCalcSineDecompositionIterative();
+    testCalcSineDecompositionRecursion();
+    printf("All testc are completed");
+    
+    return 0;
+}
