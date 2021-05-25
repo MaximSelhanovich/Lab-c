d8d764674da484c1dd9d69bae4ec37dc1d0a148b
ClGelver <75281453+MaximSelhanovich@users.noreply.github.com>
2021-05-25 14:22:15 +0300

    Add files via upload

---
+++ Lab4/Lab4_2/Lab4_2.h
@@ -0,0 +1,11 @@
+#ifndef LAB4_2_H
+#define LAB4_2_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+char charToLower(char character);
+int isLetter(const char character);
+void evenPalindrome(FILE *fileTORead, long int maxPosition);
+
+#endif
